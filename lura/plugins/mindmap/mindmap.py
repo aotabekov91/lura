4c0b148ce435d4dfca78d3f1a5edcea19e375c71
adam <uz.localteam@gmail.com>
2022-04-23 13:41:33 +0200

    mindmap

--- lura/plugins/mindmap/mindmap.py
+++ lura/plugins/mindmap/mindmap.py
@@ -3,59 +3,68 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 from .connect import DatabaseConnector
+
+from lura.core.miscel import MapTree
+
+from lura.core.miscel import Item
 from lura.core.miscel import ItemModel
 
-class MindMap(QObject):
+class MindMap(MapTree):
 
     def __init__(self, parent, settings):
-        super().__init__()
+        super().__init__(parent, parent)
         self.window=parent
+        self.location='left'
         self.name='mindmap'
+        self.globalKeys = {
+                'Ctrl+Shift+m': (
+                    self.showMindmaps,
+                    self.window,
+                    Qt.WindowShortcut),
+                }
         self.setup()
 
     def setup(self):
 
-        self.activated=False
         self.db=DatabaseConnector(self, self.window)
+        self.window.setTabLocation(self, self.location, self.name)
 
-        self.fuzzy=self.window.plugin.fuzzy
-        self.fuzzy.fuzzySelected.connect(self.on_fuzzySelected)
+    def showMindmaps(self):
+        mindmaps=self.window.plugin.tables.get('maps')
 
-        commandList=[('mo - open a map', 'openMap'),
-                    ('mc - create a map', 'createMap'),
-                    ('md - delete a map', 'deleteMap')]
+        model=ItemModel()
+        model.itemChanged.connect(self.on_itemChanged)
 
-        self.window.plugin.command.addCommands(commandList, self)
+        for m in mindmaps:
+            item=Item('map', m['id'], self.window, m['title'])
+            model.appendRow(item)
 
-    def setFuzzyData(self):
-        maps = self.window.plugin.tables.get('maps')
-        names = [m['title'] for m in maps]
-        self.fuzzy.setData(self, maps, names)
+        self.setModel(model)
+        self.window.activateTabWidget(self)
+        self.setFocus()
 
-    def openMap(self):
-        self.mode='open'
-        self.setFuzzyData()
-        self.fuzzy.activate(self)
+    def open(self):
+        item=self.currentItem()
+        if item is None: return
 
-    def createMap(self):
+        model=ItemModel(item.id(), self.window)
+
+        self.db.register(model)
+        self.window.plugin.itemviewer.open(model)
+
+    def addNode(self):
         model=ItemModel(None, self.window)
         self.db.register(model)
-        self.window.plugin.mapviewer.open(model)
 
-    def deleteMap(self):
-        self.mode='delete'
-        self.setFuzzyData()
-        self.fuzzy.activate(self)
+        self.window.plugin.itemviewer.open(model)
 
-    def on_fuzzySelected(self, selected, client):
-        if client!=self: return
-        self.fuzzy.deactivate(self)
-        if self.mode=='open':
-            model=ItemModel(selected['id'], self.window)
-            self.db.register(model)
-            self.window.plugin.itemviewer.open(model)
-        elif self.mode=='delete':
-            self.window.plugin.tables.remove(
-                    'maps', {'id': selected['id']})
-            self.setFuzzyData()
-            self.fuzzy.activate(self)
+    def delete(self):
+        item=self.currentItem()
+        if item is None: return
+
+        self.window.plugin.tables.remove('maps', {'id': item.id()})
+        self.showMindmaps()
+
+    def on_itemChanged(self, item):
+        self.window.plugin.tables.update(
+                'maps', {'id': item.id()}, {'title':item.text()})
