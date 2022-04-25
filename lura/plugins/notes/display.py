38d4acf514f4891a4a5bb720452f71850a1a6c90
adam <uz.localteam@gmail.com>
2022-04-25 20:18:52 +0200

    last

--- lura/plugins/notes/display.py
+++ lura/plugins/notes/display.py
@@ -1,6 +1,13 @@
+import sys
+
+import markdown
+import mimetypes
+import os.path
+
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtWebKit import *
 
 class Display(QWidget):
 
@@ -15,9 +22,42 @@ class Display(QWidget):
 
     def setup(self):
 
+        self.m_id=None
+
         self.m_layout=QVBoxLayout(self)
         self.m_layout.setSpacing(0)
         self.m_layout.setContentsMargins(0, 0, 0, 0)
+
+        widget=QWidget()
+        widget.m_layout=QHBoxLayout(widget)
+        widget.m_layout.setContentsMargins(0, 0, 0, 0)
+        widget.m_layout.setSpacing(0)
+
+        self.title=QLineEdit()
+        self.title.textChanged.connect(self.on_titleChanged)
+
+        self.switchButton=QPushButton('Switch')
+        self.switchButton.pressed.connect(self.on_switchButtonPressed)
+
+        self.saveButton=QPushButton('Save')
+        self.saveButton.pressed.connect(self.on_saveButtonPressed)
+
+        widget.m_layout.addWidget(self.title)
+        widget.m_layout.addWidget(self.switchButton)
+        widget.m_layout.addWidget(self.saveButton)
+
+        self.m_layout.addWidget(widget)
+
+        self.view=QStackedWidget()
+        self.content=NQWidget(self.window.plugin.tables)
+        self.web=NQWebView()
+
+        self.contentIndex=self.view.addWidget(self.content)
+        self.webIndex=self.view.addWidget(self.web)
+
+        self.view.setCurrentIndex(self.contentIndex)
+
+        self.m_layout.addWidget(self.view)
 
         self.window.setTabLocation(self, self.location, self.name)
 
@@ -28,79 +68,85 @@ class Display(QWidget):
         for i in reversed(range(self.m_layout.count())):
             self.m_layout.itemAt(i).widget().setParent(None)
 
-        self.noteWidget=NQWidget(n_id, self.window.plugin.tables)
-        self.m_layout.addWidget(self.noteWidget)
+        self.m_id=n_id
+        self.web.setId(n_id)
+        self.content.setId(n_id)
         self.window.activateTabWidget(self)
-        self.m_dockWidget.titleBarWidget().setStyleSheet('background-color: white; color: black')
 
     def toggle(self):
 
         if self.m_layout.count()==0: return
         if not self.isVisible():
             self.window.activateTabWidget(self)
+            self.m_dockWidget.titleBarWidget().setStyleSheet(
+                    'background-color: white; color: black')
         else:
             self.window.deactivateTabWidget(self)
 
-class NQWidget(QWidget):
 
-    def __init__(self, m_id, data):
+    def on_titleChanged(self, text):
+        self.window.plugin.tables.update(
+                'notes', {'id':self.m_id}, {'title':text})
+
+    def on_switchButtonPressed(self):
+        widget=self.view.currentWidget()
+        if widget==self.web:
+            self.view.setCurrentIndex(self.contentIndex)
+        else:
+            self.view.setCurrentIndex(self.webIndex)
+            self.web.load()
+
+    def on_saveButtonPressed(self):
+        self.content.save()
+
+
+class NQWidget(QTextEdit):
+
+    def __init__(self, data):
         super().__init__()
-        self.m_id=m_id
         self.m_data=data
         self.setup()
+
+    def setId(self, nid):
+        self.timer.stop()
+        self.m_id=nid
+        note=self.m_data.get('notes', {'id':self.m_id})
+        self.setPlainText(''.join(open(note['loc']).readlines()))
+        self.timer.start()
 
     def setup(self):
 
         self.changed=False
 
-        self.m_layout=QVBoxLayout(self)
-        self.m_layout.setContentsMargins(0, 0, 0, 0)
-        self.m_layout.setSpacing(0)
+        self.textChanged.connect(self.on_textChanged)
 
-        widget=QWidget()
-        widget.m_layout=QHBoxLayout(widget)
-        widget.m_layout.setContentsMargins(0, 0, 0, 0)
-        widget.m_layout.setSpacing(0)
-
-        self.title=QLineEdit()
-        self.title.textChanged.connect(self.on_titleChanged)
-
-        self.saveButton=QPushButton('Save')
-        self.saveButton.pressed.connect(self.on_saveButtonPressed)
-
-        widget.m_layout.addWidget(self.title)
-        widget.m_layout.addWidget(self.saveButton)
-        
-        self.content=QTextEdit()
-        self.content.textChanged.connect(self.on_textChanged)
         self.timer=QTimer()
         self.timer.setInterval(12000)
-        self.timer.timeout.connect(self.on_saveButtonPressed)
+        self.timer.timeout.connect(self.save)
         self.timer.start()
-
-        self.m_layout.addWidget(self.content)
-        self.m_layout.addWidget(widget)
-
-        note=self.m_data.get('notes', {'id':self.m_id})
-
-        self.title.setText(note['title'])
-        self.content.setPlainText(''.join(open(note['loc']).readlines()))
 
     def on_textChanged(self):
         self.changed=True
 
-    def on_titleChanged(self, text):
-        self.m_data.update('notes', {'id':self.m_id}, {'title':text})
-
-    def on_saveButtonPressed(self):
+    def save(self):
         if not self.isVisible(): return
         if not self.changed: return
-        text=self.content.toPlainText()
+        text=self.toPlainText()
         loc=self.m_data.get('notes', {'id':self.m_id}, 'loc')
         nFile=open(loc, 'w')
         for f in text:
             nFile.write(f)
         self.changed=False
 
-    def setFocus(self):
-        self.content.setFocus()
+class NQWebView(QWebView):
+
+    def setId(self, nid):
+        self.m_id=nid
+        note=self.m_data.get('notes', {'id':self.m_id})
+        self.m_filePath=note['loc']
+        self.load()
+
+    def load(self):
+        mFile=open(self.m_filePath)
+        mContent=mFile.read()
+        self.setHtml(markdown.markdown(mContent))
