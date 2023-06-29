e2403829836b83bbaf1bef2f6092bd338d4213a2
adam <adam@myworld.net>
2023-06-29 16:25:12 +0200

    removed package

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='lura', 
       version='1.0.0', 
       description='Lura',
-      packages=['lura.render', 'lura.core'],
       entry_points = {
           'console_scripts': [
               'lura = lura.run:main',
