9c925dc3adb91ead97920713d7a2ba5e9443b181
adam <adam@myworld.net>
2023-07-20 18:46:58 +0200

    updated the package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lura', 
-      version='1.0.0', 
+      version='0.0.1', 
       description='Lura',
       entry_points = {
           'console_scripts': [
