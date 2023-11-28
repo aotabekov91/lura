7d7ae5a3ad900307c91f3d8e36f4dac74360ab33
adam <adam@myworld.net>
2023-11-29 00:01:22 +0100

    minor

--- src/lura/main.py
+++ src/lura/main.py
@@ -17,7 +17,6 @@ class Lura(Plug):
                     plugs.Command, 
                     plugs.GreenElf,
                     plugs.Powerline,
-                    plugs.TableView,
                     ]
                 )
 
